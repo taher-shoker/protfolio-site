c55e6bb21be08d0f8434f0134a845e236846099b
taher shoker <taher-elshoker@hotmail.com>
2019-05-21 02:39:48 +0200

    Updates

---
+++ precache-manifest.a946876070a0470a68165300f1aabafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "127d5fcdf48d736c84b4",
+    "url": "/protfolio-site/static/js/runtime~main.da89357c.js"
+  },
+  {
+    "revision": "616b6b8b188c11314bb6",
+    "url": "/protfolio-site/static/js/main.a82f9a5c.chunk.js"
+  },
+  {
+    "revision": "3543fa4358aeedef3d79",
+    "url": "/protfolio-site/static/js/2.253f286b.chunk.js"
+  },
+  {
+    "revision": "616b6b8b188c11314bb6",
+    "url": "/protfolio-site/static/css/main.44a90207.chunk.css"
+  },
+  {
+    "revision": "3543fa4358aeedef3d79",
+    "url": "/protfolio-site/static/css/2.11bff923.chunk.css"
+  },
+  {
+    "revision": "e95e13a2167ffcdf024dcf091280a2a8",
+    "url": "/protfolio-site/index.html"
+  }
+];
